7fd94cbcb9181a025b065bdf3a7ea7784f9a287e
liloman <eselilo@gmail.com>
2016-03-08 20:05:55 +0100

    auto commit

--- update.sh
+++ update.sh
@@ -1,29 +1,35 @@
 #!/usr/bin/env bash
 
-#Check if local changes
-dirty() { git status --porcelain; }
+main() {
+    #function_body
+    #Check if local changes
+    dirty() { git status --porcelain; }
 
-if [[ $(dirty) ]]; then 
-    echo "dirty baby"
-    # git add .
-    # git commit -m "auto commit"
-else
-    echo "clean"
-fi
+    if [[ $(dirty) ]]; then 
+        echo "unsaved changes"
+        git add .
+        git commit -m "auto commit"
+    else
+        echo "clean"
+    fi
 
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse @{u})
-BASE=$(git merge-base @ @{u})
+    LOCAL=$(git rev-parse @)
+    REMOTE=$(git rev-parse @{u})
+    BASE=$(git merge-base @ @{u})
 
-if [[ $LOCAL = $REMOTE ]]; then
-    echo "Up-to-date"
-elif [[ $LOCAL = $BASE ]]; then
-    echo "Need to pull"
-elif [[ $REMOTE = $BASE ]]; then
-    echo "Need to push"
-else
-    echo "Diverged notify user"
-fi
+    if [[ $LOCAL = $REMOTE ]]; then
+        echo "Up-to-date"
+    elif [[ $LOCAL = $BASE ]]; then
+        echo "Need to pull"
+        git pull --rebase
+    elif [[ $REMOTE = $BASE ]]; then
+        echo "Need to push"
+        git push
+    else
+        echo "Diverged notify user"
+    fi
 
+}
 
+main
 
