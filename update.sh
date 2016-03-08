66acd03e2d207d001df6749890db02317b27cb5a
liloman <eselilo@gmail.com>
2016-03-08 20:18:38 +0100

    auto commit

--- update.sh
+++ update.sh
@@ -3,43 +3,48 @@
 readonly ROOT=~/Clones
 
 #Repos
-readonly repos="dirty dirStack"
+readonly repos="dirty dirStack checkUndocumented"
 
 #Change dir to $root
 cd $ROOT
 
-update_repo() {
-    #Check for local changes
-    dirty() { git status --porcelain; }
+main() {
+    update_repo() {
+        #Check for local changes
+        dirty() { git status --porcelain; }
 
-    if [[ $(dirty) ]]; then 
-        echo "Unsaved changes,doing commit so."
-        git add .
-        git commit -m "auto commit"
-    fi
+        if [[ $(dirty) ]]; then 
+            echo "Unsaved changes,doing commit so."
+            git add .
+            git commit -m "auto commit"
+        fi
 
-    local LOCAL=$(git rev-parse @)
-    local REMOTE=$(git rev-parse @{u})
-    local BASE=$(git merge-base @ @{u})
+        local LOCAL=$(git rev-parse @)
+        local REMOTE=$(git rev-parse @{u})
+        local BASE=$(git merge-base @ @{u})
 
-    if [[ $LOCAL = $REMOTE ]]; then
-        echo "Up-to-date"
-    elif [[ $LOCAL = $BASE ]]; then
-        echo "Need to pull"
-        git pull --rebase
-    elif [[ $REMOTE = $BASE ]]; then
-        echo "Need to push"
-        git push
-    else
-        echo "Diverged notify user"
-    fi
+        if [[ $LOCAL = $REMOTE ]]; then
+            echo "Up-to-date"
+        elif [[ $LOCAL = $BASE ]]; then
+            echo "Need to pull"
+            git pull --rebase
+        elif [[ $REMOTE = $BASE ]]; then
+            echo "Need to push"
+            git push
+        else
+            echo "Diverged notify user"
+        fi
+    }
+
+
+    for dir in $repos; do
+        [[ ! -e $dir ]] && die "$dir not found"
+        echo "**********************************"
+        echo "Doing $dir"
+        cd $dir && update_repo
+        cd $ROOT
+    done
+
 }
 
-
-for dir in $repos; do
-    echo "**********************************"
-    echo "Doing $dir"
-    cd $dir && update_repo
-    cd $ROOT
-done
-
+main
